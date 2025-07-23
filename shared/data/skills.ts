c73811d690a72fceda9db0dd1ef0fce0f135e06e
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 19:39:07 -0500

    Update skills to use localization

--- shared/data/skills.ts
+++ shared/data/skills.ts
@@ -2,27 +2,51 @@ import type { Skill } from "../schemas/skill";
 
 export const SKILLS: Skill[] = [
   {
-    category: "Programming Languages",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "Programming Languages"
+    },
     skills: ["TypeScript", "Python", "Rust", "Java", "Swift"],
   },
   {
-    category: "Data & Databases",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "Data & Databases"
+    },
     skills: ["Polars", "SQL", "Plotly", "Jupyter", "Hex", "PostgreSQL", "Redis", "Oracle SQL Developer"],
   },
   {
-    category: "Cloud & DevOps",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "Cloud & DevOps"
+    },
     skills: ["AWS (RDS, ECS, S3, DynamoDB)", "Docker", "Coolify", "GitHub CI/CD"],
   },
   {
-    category: "Web Technologies",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "Web Technologies"
+    },
     skills: ["React", "Svelte", "Next.js", "FastAPI", "Fastify", "Spring Boot", "HTML", "CSS", "MUI", "ShadCN"],
   },
   {
-    category: "AI/ML",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "AI/ML"
+    },
     skills: [ "TensorFlow", "PyTorch", "Anthropic API", "OpenAI API", "Groq API", "Gemini API"],
   },
   {
-    category: "Other",
+    showInCV: true,
+    showInResume: true,
+    category: {
+      en: "Other"
+    },
     skills: ["LaTeX", "Git", "Bash", "Linux", "Firebase"],
   },
 ]; 
