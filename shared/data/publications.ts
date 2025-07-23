63f52048b2345854be903ef917cbb43120a97c5b
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 19:28:11 -0500

    Update publications to use localization

--- shared/data/publications.ts
+++ shared/data/publications.ts
@@ -3,7 +3,11 @@ import type { Publication } from "../schemas/publication";
 
 export const PUBLICATIONS: Publication[] = [
   {
-    title: "Desarrollo del Perfil del estudiante dentro de No estás solo",
+    showInCV: true,
+    showInResume: true,
+    title: {
+      en: "Desarrollo del Perfil del estudiante dentro de No estás solo"
+    },
     authors: [
       {
         name: "Melo Barrero, F.",
@@ -11,10 +15,21 @@ export const PUBLICATIONS: Publication[] = [
       }
     ],
     year: 2024,
-    description: "Undergraduate thesis presented at Universidad de los Andes to obtain the degree of Systems and Computing Engineer",
+    description: {
+      en: {
+        full: "Undergraduate thesis presented at Universidad de los Andes to obtain the degree of Systems and Computing Engineer",
+        short: "Undergraduate thesis - Systems and Computing Engineering degree"
+      },
+      es: {
+        full: "Tesis de pregrado presentada en Universidad de los Andes para obtener el título de Ingeniero de Sistemas y Computación",
+        short: "Tesis de pregrado - Ingeniería de Sistemas y Computación"
+      }
+    },
     institution: "Universidad de los Andes",
     url: "https://repositorio.uniandes.edu.co/entities/publication/26656ec1-50e7-42cd-976c-1cc0194beb5a",
-    linkText: "Uniandes Repository",
+    linkText: {
+      en: "Uniandes Repository"
+    },
     ...UNIANDES,
   },
 ]; 
