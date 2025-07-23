c535af9b3bd218cfd0e5a0a3efb32fd75b4a3271
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 19:38:15 -0500

    Update relevant coursework to use localization

--- shared/data/relevantCoursework.ts
+++ shared/data/relevantCoursework.ts
@@ -2,33 +2,63 @@ import type { RelevantCoursework } from "../schemas/relevantCoursework";
 
 export const RELEVANT_COURSEWORK: RelevantCoursework[] = [
   {
-    area: "Computer Science",
+    showInCV: true,
+    showInResume: false,
+    area: {
+      en: "Computer Science"
+    },
     courses: [
       {
-        name: "Languages and Automata",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Languages and Automata",
+          es: "Lenguajes y Máquinas"
+        },
         originalName: "Lenguajes y Máquinas",
         code: "ISIS-1106",
         credits: 3
       },
       {
-        name: "Reinforcement Learning",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Reinforcement Learning",
+          es: "Aprendizaje por Refuerzo"
+        },
+        originalName: "Reinforcement Learning",
         code: "ISIS-4222",
         credits: 4
       },
       {
-        name: "Modeling, Simulation and Optimization",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Modeling, Simulation and Optimization",
+          es: "Modelado, Simulación y Optimización"
+        },
         originalName: "Modelado, Simulación y Optimización",
         code: "ISIS-3302",
         credits: 3
       },
       {
-        name: "Algorithm Design and Analysis",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Algorithm Design and Analysis",
+          es: "Diseño y Análisis de Algoritmos"
+        },
         originalName: "Diseño y Análisis de Algoritmos",
         code: "ISIS-1105",
         credits: 3
       },
       {
-        name: "Data Structures and Algorithms",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Data Structures and Algorithms",
+          es: "Estructuras de Datos y Algoritmos"
+        },
         originalName: "Estructuras de Datos y Algoritmos",
         code: "ISIS-1225",
         credits: 3
@@ -36,91 +66,187 @@ export const RELEVANT_COURSEWORK: RelevantCoursework[] = [
     ]
   },
   {
-    area: "Mathematics",
+    showInCV: true,
+    showInResume: false,
+    area: {
+      en: "Mathematics"
+    },
     courses: [
       {
-        name: "Graph Theory",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Graph Theory",
+          es: "Teoría de Grafos"
+        },
         originalName: "Teoría de Grafos",
         code: "MATE-1104",
         credits: 3
       },
       {
-        name: "Game Theory",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Game Theory",
+          es: "Teoría de Juegos"
+        },
         originalName: "Teoría de Juegos",
         code: "MATE-3712",
         credits: 3
       },
       {
-        name: "Computational Methods",
-        originalName: "Métodos Computacionales 1",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Computational Methods",
+          es: "Métodos Computacionales"
+        },
+        originalName: "Métodos Computacionales",
         code: "FISI-2526",
         credits: 3
       },
       {
-        name: "Structural Mathematics",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Structural Mathematics",
+          es: "Matemática Estructural"
+        },
         originalName: "Matemática Estructural",
         code: "MATE-1102",
         credits: 3
       },
       {
-        name: "Probability and Statistics",
-        originalName: "Probabilidad y Estadística 1",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Statistics and Probability",
+          es: "Probabilidad y Estadística"
+        },
+        originalName: "Probabilidad y Estadística",
         code: "IIND-2106",
         credits: 3
       },
       {
-        name: "Linear Algebra",
-        originalName: "Álgebra Lineal 1",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Linear Algebra",
+          es: "Álgebra Lineal"
+        },
+        originalName: "Álgebra Lineal",
         code: "MATE-1105",
         credits: 3
       },
       {
-        name: "Calculus I, II, III",
-        originalName: "Cálculo Diferencial, Cálculo Integral y Cálculo Vectorial",
-        code: "MATE-1203/1214/1207",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Vector Calculus",
+          es: "Cálculo Vectorial"
+        },
+        originalName: "Cálculo Vectorial",
+        code: "MATE-1207",
         credits: 3
-      }
+      },
+      {
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Integral Calculus",
+          es: "Cálculo Integral"
+        },
+        originalName: "Cálculo Integral",
+        code: "MATE-1214",
+        credits: 3
+      },
+      {
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Differential Calculus",
+          es: "Cálculo Diferencial"
+        },
+        originalName: "Cálculo Diferencial",
+        code: "MATE-1203",
+        credits: 3
+      },
     ]
   },
   {
-    area: "Software Engineering",
+    showInCV: true,
+    showInResume: false,
+    area: {
+      en: "Software Engineering"
+    },
     courses: [
       {
-        name: "Software Metrics and Quality",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Software Metrics and Quality",
+          es: "Métricas y Calidad de Software"
+        },
         originalName: "Métricas y Calidad de Software",
         code: "MISW-4404",
         credits: 2
       },
       {
-        name: "Software Architecture and Design",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Software Architecture and Design",
+          es: "Arquitectura y Diseño de Software"
+        },
         originalName: "Arquitectura y Diseño de Software",
         code: "ISIS-2503",
         credits: 3
       },
       {
-        name: "Web Technologies Programming",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Web Technologies Programming",
+          es: "Programación con Tecnologías Web"
+        },
         originalName: "Programación con Tecnologías Web",
         code: "ISIS-3710",
         credits: 3
       },
       {
-        name: "API Design and Construction",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "API Design and Construction",
+          es: "Diseño y Construcción de APIs"
+        },
         originalName: "Diseño y Construcción de APIs",
         code: "MISW-4403",
         credits: 2
       },
       {
-        name: "Team Software Development",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Team Software Development",
+          es: "Desarrollo de Software en Equipo"
+        },
         originalName: "Desarrollo de Software en Equipo",
         code: "ISIS-2603",
         credits: 3
       },
       {
-        name: "Transactional Systems",
+        showInCV: true,
+        showInResume: false,
+        name: {
+          en: "Transactional Systems",
+          es: "Sistemas Transaccionales"
+        },
         originalName: "Sistemas Transaccionales",
         code: "ISIS-2304",
         credits: 3
-      }
+      },
+
     ]
   }
 ];
