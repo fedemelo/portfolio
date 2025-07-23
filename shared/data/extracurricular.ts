0ac1f4eece08984e7011d9defe4028b5e8055aa9
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 19:27:14 -0500

    Update extracurricular to use localization

--- shared/data/extracurricular.ts
+++ shared/data/extracurricular.ts
@@ -2,9 +2,27 @@ import type { Extracurricular } from "../schemas/extracurricular";
 
 export const EXTRACURRICULARS: Extracurricular[] = [
   {
-    description: "National-level swimmer",
+    showInCV: true,
+    showInResume: true,
+    description: {
+      en: {
+        full: "National-level swimmer",
+      },
+      es: {
+        full: "Nadador de nivel nacional",
+      }
+    },
     events: [
-      "Universidad de los Andes Swimming Team member and Captain, 4-time Bogotá City Swimming Team member, 12 National Swimming Championships",  // 12: 4 Interligas, 4 Interclubes, 3 ASCUN, 1 Cerros
+      {
+        en: {
+          full: "Universidad de los Andes Swimming Team member and Captain, 4-time Bogotá City Swimming Team member, 12 National Swimming Championships",
+          short: "Uniandes Swimming Team Captain, 4-time Bogotá City Team member, 12 National Championships"
+        },
+        es: {
+          full: "Miembro de la Selección de Natación de la Universidad de los Andes, 4 veces miembro de la Selección Bogotá de Natación, 12 campeonatos nacionales de Natación",
+          short: "Capitán Selección de Natación Uniandes, 4 veces Selección Bogotá de Natación, 12 campeonatos nacionales"
+        }
+      }
     ],
   },
 ];
