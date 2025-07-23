c7d5cca1832c87f6dad87ec5641a8fb2178d7b75
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 19:38:52 -0500

    Update research interests to use localization

--- shared/data/researchInterests.ts
+++ shared/data/researchInterests.ts
@@ -1,9 +1,11 @@
 import type { ResearchInterest } from "../schemas/researchInterest";
 
 export const RESEARCH_INTERESTS: ResearchInterest = {
-  text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean venenatis ex sit amet leo aliquam rhoncus. Morbi ut ante nisi. Sed dictum mauris justo, et convallis nisi ultricies at. Mauris a dignissim odio. In faucibus orci id ante posuere, et sollicitudin lacus eleifend. Duis eu massa euismod, semper orci quis, aliquet risus. Nullam ut ex eget quam condimentum aliquam. Nullam luctus sapien quis enim ultrices pulvinar.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas euismod hendrerit tortor. Duis malesuada elit vitae enim luctus porta. Duis tempus faucibus nisi eget sodales. Phasellus convallis varius enim, in aliquet urna posuere vel. Fusce semper nibh vitae iaculis lobortis. Sed hendrerit nunc
-`,
+  text: {
+    en: {
+      full: `TODO`
+    }
+  },
   showInCV: false,
+  showInResume: false,
 }
