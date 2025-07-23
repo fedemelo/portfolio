6dc9523973e6837581d9ef9295d024121d27c24f
Federico Melo <f.melo@uniandes.edu.co>
2025-07-22 20:05:32 -0500

    Update awards to use localization

--- shared/data/awards.ts
+++ shared/data/awards.ts
@@ -3,47 +3,102 @@ import type { Award } from "../schemas/award";
 
 export const AWARDS: Award[] = [
   {
-    title: "Summa Cum Laude",
-    description: `Summa Cum Laude is the highest undergraduate academic honor. Eligibility requires a cumulative GPA within the top 1% of graduates from the faculty over the past five years, and demonstrated integral merits in an interview before a commission appointed by the Academic Council.`,
+    title: {
+      en: "Summa Cum Laude",
+    },
+    description: {
+      en: {
+        full: `Summa Cum Laude is the highest undergraduate academic honor. Eligibility requires a cumulative GPA within the top 1% of graduates from the faculty over the past five years, and demonstrated integral merits in an interview before a commission appointed by the Academic Council.`,
+      },
+    },
     date: new Date("2025-04-07T12:00:00-05:00"),
+    showInCV: true,
+    showInResume: false,
     ...UNIANDES,
   },
   {
-    title: "Athletic Career Distinction",
-    description: `The Athletic Career Distinction is awarded to a single student in the graduating class for exemplary leadership, commitment, and ethics in sports. Awarded citing 41 swimming medals, including 24 gold, and two years as elected team captain.`,
+    title: {
+      en: "Athletic Career Distinction",
+      es: "Distinción a la Trayectoria Deportiva"
+    },
+    description: {
+      en: {
+        full: `The Athletic Career Distinction is awarded to a single student in the graduating class for exemplary leadership, commitment, and ethics in sports. Awarded citing 41 swimming medals, including 24 gold, and two years as elected team captain.`,
+      },
+    },
     date: new Date("2025-04-07T12:00:00-05:00"),
+    showInCV: true,
+    showInResume: false,
     ...UNIANDES,
   },
   {
-    title: "Ramón de Zubiría Awardee",
-    description: `Recognition awarded annually to the undergraduate student with the highest cumulative GPA among all students in their major.`,
+    title: {
+      en: "Ramón de Zubiría Awardee",
+      es: "Distinción Ramón de Zubiría"
+    },
+    description: {
+      en: {
+        full: `Recognition awarded annually to the undergraduate student with the highest cumulative GPA among all students in their major.`,
+      },
+    },
+    showInCV: true,
+    showInResume: false,
     ...UNIANDES,
     instances: [
       {
-        description: `Cumulative GPA of 4.90 / 5.00 in Systems and Computing Engineering`,
+        description: {
+          en: {
+            full: `Cumulative GPA of 4.90 / 5.00 in Systems and Computing Engineering`,
+          },
+        },
         date: new Date("2023-11-15T12:00:00-05:00"),
       }, 
       {
-        description: `Cumulative GPA of 4.91 / 5.00 in Systems and Computing Engineering`,
+        description: {
+          en: {
+            full: `Cumulative GPA of 4.91 / 5.00 in Systems and Computing Engineering`,
+          },
+        },
         date: new Date("2024-11-13T12:00:00-05:00"),
       }, 
     ],
   },
   {
-    title: "Semester Excellence Awardee",
-    description: `Recognition awarded to the student who achieved the highest semester GPA in their undergraduate program during the immediately preceding semester.`,
+    title: {
+      en: "Semester Excellence Awardee",
+      es: "Premio a la Excelencia Semestral"
+    },
+    description: {
+      en: {
+        full: `Recognition awarded to the student who achieved the highest semester GPA in their undergraduate program during the immediately preceding semester.`,
+      },
+    },
+    showInCV: true,
+    showInResume: false,
     ...UNIANDES,
     instances: [
       {
-        description: `Semester GPA of 4.90 / 5.00 in Physics`,
+        description: {
+          en: {
+            full: `Semester GPA of 4.90 / 5.00 in Physics`,
+          },
+        },
         date: new Date("2021-06-30T12:00:00-05:00"),
       },
       {
-        description: `Semester GPA of 4.86 / 5.00 in Physics`,
+        description: {
+          en: {
+            full: `Semester GPA of 4.86 / 5.00 in Physics`,
+          },
+        },
         date: new Date("2021-11-30T12:00:00-05:00"),
       },
       {
-        description: `Semester GPA of 4.97 / 5.00 in Systems and Computing Engineering`,
+        description: {
+          en: {
+            full: `Semester GPA of 4.97 / 5.00 in Systems and Computing Engineering`,
+          },
+        },
         date: new Date("2023-11-15T12:00:00-05:00"),
       },
     ],
