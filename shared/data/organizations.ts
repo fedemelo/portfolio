a545982de386881c9416e32109b7dada35c586af
Federico Melo <f.melo@uniandes.edu.co>
2025-11-23 00:24:38 -0500

    Create organization constants with icon and link data

---
+++ shared/data/organizations.ts
@@ -0,0 +1,26 @@
+import type { Organization } from "../schemas/organization";
+
+export const UNIANDES: Organization = {
+  name: "Universidad de los Andes",
+  icon: "/icons/uniandes-icon.png",
+  link: "https://uniandes.edu.co",
+};
+
+export const CANALS_AI: Organization = {
+  name: "Canals AI",
+  icon: "/icons/canals-icon.png",
+  link: "https://canals.ai",
+};
+
+export const CASEWARE: Organization = {
+  name: "Caseware International Inc.",
+  icon: "/icons/caseware-icon.png",
+  link: "https://www.caseware.com",
+};
+
+export const COLEGIO_SAN_CARLOS: Organization = {
+  name: "Colegio San Carlos",
+  icon: "/icons/csc-icon.png",
+  link: "https://www.colegiosancarlos.edu.co",
+};
+
