1109229fcc5d2c64dc759b82a79916961b14e421
Federico Melo <f.melo@uniandes.edu.co>
2025-12-20 16:57:29 -0500

    Implement description and achievements filtering in CV

--- cv/src/components/Achievements.svelte
+++ cv/src/components/Achievements.svelte
@@ -2,21 +2,25 @@
   import type { WorkExperience } from "../../../shared/schemas/workExperience";
   import type { Teaching } from "../../../shared/schemas/teaching";
   import { getCVText } from "../../../shared/utils/localization";
+  import { filterForCV } from "../../../shared/utils/show";
   import { getContext } from 'svelte';
   import type { Language } from "../../../shared/schemas/utils";
   
   export let experience: WorkExperience | Teaching;
   
   const language = getContext<Language>('language');
+  // Show description in the CV by default, unless explicitly instructed not to
+  $: showDescription = experience.description?.showInCV ?? true;
+  $: filteredAchievements = filterForCV(experience.achievements ?? []);
 </script>
 
 <div class="indented-block">
-  {#if experience.description}
+  {#if showDescription && experience.description}
     <p>{getCVText(experience.description, language)}</p>
   {/if}
   <ul>
-    {#if experience.achievements?.length}
-      {#each experience.achievements as achievement}
+    {#if filteredAchievements.length > 0}
+      {#each filteredAchievements as achievement}
         <li>{getCVText(achievement, language)}</li>
       {/each}
     {/if}
