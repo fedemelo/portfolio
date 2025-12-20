717a1133cfada64c80c5d7e1e7ddda593df33b78
Federico Melo <f.melo@uniandes.edu.co>
2025-12-20 16:45:28 -0500

    Extend teaching schema

--- shared/schemas/teaching.ts
+++ shared/schemas/teaching.ts
@@ -1,11 +1,19 @@
 import { z } from "zod";
 import { HideableSchema } from "./hideable";
+import { LocationSchema } from "./location";
+import { OrganizationSchema } from "./organization";
 import { RichLocalizedContentSchema, SimpleLocalizedContentSchema } from "./utils";
+
+export const TeachingTypeSchema = z.enum(["professional", "undergraduate"]);
+export type TeachingType = z.infer<typeof TeachingTypeSchema>;
 
 export const TeachingSchema = z.object({
   ...HideableSchema.shape,
+  ...LocationSchema.shape,
   id: z.string(),
+  type: TeachingTypeSchema,
   title: SimpleLocalizedContentSchema,
+  organization: OrganizationSchema,
   courseCode: z.string(),
   period: z.string(),
   startDate: z.date(),
