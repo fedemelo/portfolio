446ec8432abf6eef7cc2d8e4e75f659fe71801af
Federico Melo <f.melo@uniandes.edu.co>
2026-05-14 18:34:44 -0500

    Use grouping in CV work exp

--- cv/src/sections/WorkExperience.svelte
+++ cv/src/sections/WorkExperience.svelte
@@ -5,44 +5,78 @@
   import Achievements from "../components/Achievements.svelte";
   import { filterForCV } from "../../../shared/utils/show";
   import { getLocalizedText } from "../../../shared/utils/localization";
+  import { groupByGroupId } from "../../../shared/utils/group-by-group-id";
   import { getContext } from 'svelte';
   import type { Language } from "../../../shared/schemas/utils";
-  
+
   export let workExperience: WorkExperience[];
-  
+
   const language = getContext<Language>('language');
+
+  const grouped = groupByGroupId(filterForCV(workExperience));
 </script>
 
 <section>
   <h2>Work Experience</h2>
-  {#each filterForCV(workExperience) as work}
-    <div class="no-break-on-print">
-      <div class="row">
-        <h3>{work.organization.name}</h3>
-        <Location
-          location={{
-            city: work.city,
-            state: work.state,
-            country: work.country,
-          }}
-          workMode={work.workMode}
-        />
-      </div>
-      <div class="row">
-        <p style="font-style: italic;">
-          {getLocalizedText(work.title, language)}
-          {#if work.squad && work.team}
-            <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(work.squad, language)}, {getLocalizedText(work.team, language)}
-          {:else if work.team}
-            <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(work.team, language)} 
+  {#each grouped as entry}
+    {#if entry.type === 'group'}
+      {#each entry.items as work, i}
+        <div class="no-break-on-print">
+          {#if i === 0}
+            <div class="row">
+              <h3>{entry.items[0].organization.name}</h3>
+              <Location
+                location={{
+                  city: entry.items[0].city,
+                  state: entry.items[0].state,
+                  country: entry.items[0].country,
+                }}
+                workMode={entry.items[0].workMode}
+              />
+            </div>
           {/if}
-          
-        </p>
-        <p>
-          {getYearRange(work.startDate, work.endDate, work.isCurrent)}
-        </p>
+          <div class="row">
+            <p style="font-style: italic;">
+              {getLocalizedText(work.title, language)}
+              {#if work.squad && work.team}
+                <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(work.squad, language)}, {getLocalizedText(work.team, language)}
+              {:else if work.team}
+                <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(work.team, language)}
+              {/if}
+            </p>
+            <p>{getYearRange(work.startDate, work.endDate, work.isCurrent)}</p>
+          </div>
+          <Achievements experience={work} />
+        </div>
+      {/each}
+    {:else}
+      <div class="no-break-on-print">
+        <div class="row">
+          <h3>{entry.item.organization.name}</h3>
+          <Location
+            location={{
+              city: entry.item.city,
+              state: entry.item.state,
+              country: entry.item.country,
+            }}
+            workMode={entry.item.workMode}
+          />
+        </div>
+        <div class="row">
+          <p style="font-style: italic;">
+            {getLocalizedText(entry.item.title, language)}
+            {#if entry.item.squad && entry.item.team}
+              <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(entry.item.squad, language)}, {getLocalizedText(entry.item.team, language)}
+            {:else if entry.item.team}
+              <span style="font-style: normal; margin: 0 3pt;">•</span>{getLocalizedText(entry.item.team, language)}
+            {/if}
+          </p>
+          <p>
+            {getYearRange(entry.item.startDate, entry.item.endDate, entry.item.isCurrent)}
+          </p>
+        </div>
+        <Achievements experience={entry.item} />
       </div>
-    </div>
-    <Achievements experience={work} />
+    {/if}
   {/each}
 </section>
