255002d7d96183e107085076d78f4d096f0b25f2
Federico Melo <f.melo@uniandes.edu.co>
2026-05-15 00:10:46 -0500

    Get org name conditionally

--- webpage/types/organization.ts
+++ webpage/types/organization.ts
@@ -1,6 +1,11 @@
 export interface Organization {
-  name: string
+  name: string | { en: string; es?: string }
   icon?: string
   link?: string
+}
+
+export function getOrgName(org: Organization): string {
+  if (typeof org.name === 'string') return org.name;
+  return org.name.en;
 }
 
