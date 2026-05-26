a41a0f2601e68866400fd0b18acd7df71db48da5
Federico Melo <f.melo@uniandes.edu.co>
2026-05-26 01:47:11 -0500

    Show only years for award instances in CV

--- cv/src/sections/Awards.svelte
+++ cv/src/sections/Awards.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import type { Award } from "../../../shared/schemas/award";
   import { filterForCV } from "../../../shared/utils/show";
-  import { getYearRange } from "../../../shared/utils/year";
-  import { getPeriodFromDate } from "../../../shared/utils/period";
+  import { getYearRange, getYearSequence } from "../../../shared/utils/year";
   import { getLocalizedText, getCVText, getOrgName } from "../../../shared/utils/localization";
   import { getContext } from 'svelte';
   import type { Language } from "../../../shared/schemas/utils";
@@ -16,28 +15,27 @@
   <h2>Awards and Honors</h2>
   {#each filterForCV(awards) as award}
     <div class="no-break-on-print">
-      <h3>{getLocalizedText(award.title, language)}</h3>
-      <div class="row">
-        <p>{getOrgName(award.organization, language)}</p>
-        {#if award.date}
-          <p>{getYearRange(award.date)}</p>
-        {/if}
-      </div>
-      <div class="indented-block">
-        <p>{getCVText(award.description, language)}</p>
-        {#if award.instances}
-          <ul>
-            {#each award.instances as instance}
-              <li>
-                <div class="row">
-                  <span>{getCVText(instance.description, language)}</span>
-                  <span>{getPeriodFromDate(instance.date)}</span>
-                </div>
-              </li>
-            {/each}
-          </ul>
-        {/if}
-      </div>
+      {#if award.instances}
+        <h3>{getLocalizedText(award.title, language)} ({award.instances.length}×)</h3>
+        <div class="row">
+          <p>{getOrgName(award.organization, language)}</p>
+          <p>{getYearSequence(award.instances.map(i => i.date))}</p>
+        </div>
+        <div class="indented-block">
+          <p>{getCVText(award.description, language)}</p>
+        </div>
+      {:else}
+        <h3>{getLocalizedText(award.title, language)}</h3>
+        <div class="row">
+          <p>{getOrgName(award.organization, language)}</p>
+          {#if award.date}
+            <p>{getYearRange(award.date)}</p>
+          {/if}
+        </div>
+        <div class="indented-block">
+          <p>{getCVText(award.description, language)}</p>
+        </div>
+      {/if}
     </div>
   {/each}
 </section>
